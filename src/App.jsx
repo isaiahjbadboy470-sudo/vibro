a8f4e70eefd84b27f90337bddc7bbf1e31657277
isaiahjbadboy470-sudo <isaiahjbadboy470@gmail.com>
2025-10-28 18:27:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Video from "./pages/Video";
+import VideoPage from "./pages/Video";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import UploadVideo from "./pages/UploadVideo";
@@ -10,11 +8,10 @@ import UploadVideo from "./pages/UploadVideo";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/video/:id" element={<Video />} />
-        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/c/:videoId" element={<VideoPage />} />
+        <Route path="/profile/:userId" element={<Profile />} />
         <Route path="/register" element={<Register />} />
         <Route path="/upload" element={<UploadVideo />} />
       </Routes>
